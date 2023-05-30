b79eef896e599373c6ba5e9797a89517f3b2c21e
tokisaki189 <andt@runsystem.net>
2023-05-30 17:44:01 +0700

    commit y nghia

--- testread.py
+++ testread.py
@@ -1,18 +1,23 @@
 import openpyxl
 import openai
 openai.api_key = ""
-input_file = './data/Testcase_JP.xlsx'
+input_file = './data/Spec_JP.xlsx'
 workbook = openpyxl.load_workbook(input_file)
 sheet = workbook.active
 for row in sheet.iter_rows():
     for cell in row:
-        if cell.value:
+        if cell.value is not None and isinstance(cell.value, str) and not cell.value.isascii() and '=' not in cell.value:
+            print (cell.value)
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": ("translate this japanese to vietnamese (if you see special character, english or you cant translate, response exactly what I input): [" + cell.value + "]"),},
+                messages=[{"role": "user", "content": ("translate this japanese to english : " + cell.value ),},
                 ]
             )
             modified_content = response['choices'][0]['message']['content']
             cell.value = modified_content
+        elif isinstance(cell.value, int):
+            continue
+        elif cell.value is not None and '=' in cell.value:
+            cell.value = cell.value
 output_file = 'output.xlsx'
 workbook.save(output_file)
