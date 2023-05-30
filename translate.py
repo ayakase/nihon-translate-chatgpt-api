00171512fc7265d0f570728e4704f172c9c81429
tokisaki189 <andt@runsystem.net>
2023-05-30 17:48:01 +0700

    doi ten

--- translate.py
+++ translate.py
@@ -1,6 +1,6 @@
 import openpyxl
 import openai
-openai.api_key = "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"
+openai.api_key = "sk-"
 input_file = './data/Spec_JP.xlsx'
 workbook = openpyxl.load_workbook(input_file)
 sheet = workbook.active
