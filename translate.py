ccd1ef8b6ae7b254e0ae41ded93a40d0cb808199
ayakase <wibu1892001@gmail.com>
2023-05-31 13:56:24 +0700

    loop through all sheet

--- translate.py
+++ translate.py
@@ -1,23 +1,26 @@
 import openpyxl
 import openai
 openai.api_key = ""
-input_file = './data/Spec_JP.xlsx'
+input_file = './data.xlsx'
 workbook = openpyxl.load_workbook(input_file)
-sheet = workbook.active
-for row in sheet.iter_rows():
-    for cell in row:
-        if cell.value is not None and isinstance(cell.value, str) and not cell.value.isascii() and '=' not in cell.value:
-            print (cell.value)
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": ("translate this japanese to english : " + cell.value ),},
+
+sheet_names = workbook.sheetnames
+for sheet_name in sheet_names:
+    sheet = workbook[sheet_name]
+    for row in sheet.iter_rows():
+        for cell in row:
+            if cell.value is not None and isinstance(cell.value, str) and not cell.value.isascii() and '=' not in cell.value:
+                print (cell.value)
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[{"role": "user", "content": ("translate this japanese to english : " + cell.value ),},
                 ]
             )
-            modified_content = response['choices'][0]['message']['content']
-            cell.value = modified_content
-        elif isinstance(cell.value, int):
-            continue
-        elif cell.value is not None and '=' in cell.value:
-            cell.value = cell.value
+                modified_content = response['choices'][0]['message']['content']
+                cell.value = modified_content
+            elif isinstance(cell.value, int):
+                continue
+            else:
+                continue
 output_file = 'output.xlsx'
 workbook.save(output_file)
